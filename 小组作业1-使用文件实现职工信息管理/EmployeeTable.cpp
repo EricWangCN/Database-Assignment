67050754f96196e05f98ca2c4d2cda59db5e7e8b
EricWangCN <wangzilong2016@outlook.com>
2020-09-14 00:59:43 +0800

    完善作业

--- 小组作业1-使用文件实现职工信息管理/EmployeeTable.cpp
+++ 小组作业1-使用文件实现职工信息管理/EmployeeTable.cpp
@@ -4,15 +4,22 @@
 
 #include "EmployeeTable.h"
 
-bool EmployeeTable::readFile() {
+bool EmployeeTable::readFile()
+{
     string sName, sPos, sTmp;
     sex sSex;
     int iID;
     file.open("EmployeeInfo.txt", ios::in);
-    while (!file.eof()) {
+    while (!file.eof())
+    {
         file >> sName >> iID >> sPos >> sTmp;
-        if (sTmp == "F") sSex = F;
-        else sSex = M;
+        // cout << sName << iID << sPos << sTmp;
+        if (sName == "")
+            break;
+        if (sTmp == "F")
+            sSex = F;
+        else
+            sSex = M;
         sEmployee tmp = sEmployee(sName, iID, sPos, sSex);
         Employees.push_back(tmp);
     }
@@ -21,64 +28,87 @@ bool EmployeeTable::readFile() {
     return true;
 }
 
-int EmployeeTable::search(const int &id) {
+int EmployeeTable::search(const int &id)
+{
     int iLen = Employees.size();
-    for (int i = 0; i < iLen; ++i) {
-        if (Employees[i].iID == id) return i;
+    for (int i = 0; i < iLen; ++i)
+    {
+        if (Employees[i].iID == id)
+            return i;
     }
     return -1;
 }
 
-bool EmployeeTable::addEmployee(sEmployee e) {
-    if (search(e.iID) == -1) {
+bool EmployeeTable::addEmployee(sEmployee e)
+{
+    if (search(e.iID) == -1)
+    {
         Employees.push_back(e);
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
-void EmployeeTable::print() {
+void EmployeeTable::print()
+{
     int iLen = Employees.size();
     cout << "Name\tID\tPosition\n";
     for (int i = 0; i < iLen; ++i)
         cout << Employees[i].sName << '\t' << Employees[i].iID << '\t' << Employees[i].sPosition << '\n';
 }
 
-bool EmployeeTable::changeInfo(const int &id, string position) {
+bool EmployeeTable::changeInfo(const int &id, string position)
+{
     int iPos = search(id);
-    if (iPos >= 0) {
+    if (iPos >= 0)
+    {
         Employees[iPos].sPosition = position;
         return true;
-    } else // Cannot find him/her
+    }
+    else // Cannot find him/her
         return false;
 }
 
-bool EmployeeTable::deleteEmployee(const int &id) {
+bool EmployeeTable::deleteEmployee(const int &id)
+{
     int iPos = search(id), iLen = Employees.size();
-    if (iPos >= 0) {
-        for (int i = iPos; i < iLen - 1; ++i) {
+    if (iPos >= 0)
+    {
+        for (int i = iPos; i < iLen - 1; ++i)
+        {
             Employees[i].sPosition = Employees[i + 1].sPosition;
             Employees[i].iID = Employees[i + 1].iID;
             Employees[i].sName = Employees[i + 1].sName;
-
         }
         Employees.pop_back();
         return true;
-    } else
+    }
+    else
         return false;
 }
 
-void EmployeeTable::writeFile() {
+void EmployeeTable::writeFile()
+{
     fstream ile;
     int iLen = Employees.size();
     string sTmp;
     ile.open("EmployeeInfo.txt", ios::out);
-    if (!ile.is_open()) cout<<"error\n";
-    for (int i = 0; i < iLen; ++i) {
-        if (Employees[i].Sex == F) sTmp = "F";
-        else sTmp = "M";
-        ile << Employees[i].sName << ' ' << Employees[i].iID << ' ' << Employees[i].sPosition << ' ' << sTmp << '\n';
+    if (!ile.is_open())
+        cout << "error\n";
+    for (int i = 0; i < iLen; ++i)
+    {
+        if (Employees[i].Sex == F)
+            sTmp = "F";
+        else
+            sTmp = "M";
+        ile << Employees[i].sName << ' ' << Employees[i].iID << ' ' << Employees[i].sPosition << ' ' << sTmp;
+        if (i != iLen - 1)
+        {
+            ile << '\n';
+        }
     }
     ile.close();
 }
--- 小组作业1-使用文件实现职工信息管理/main.cpp
+++ 小组作业1-使用文件实现职工信息管理/main.cpp
@@ -16,10 +16,10 @@ using namespace std;
 int printMenu(int options = 7)
 {
     int cmd;
-    cout << "请输入需要进行操作的序号：" << endl;
+    cout << "\n\n\n===================================\n请输入需要进行操作的序号：" << endl;
     cout << "1. 从文件中读入员工信息" << endl;
     cout << "2. 添加员工入职信息" << endl;
-    cout << "3. 查找指定员工" << endl;
+    cout << "3. 查找指定员工是否在职" << endl;
     cout << "4. 修改员工信息" << endl;
     cout << "5. 办理员工离职" << endl;
     cout << "6. 查看全部员工名单" << endl;
@@ -48,71 +48,86 @@ int main()
     {
         switch (cmd)
         {
-            case 1:
-                table.readFile();
-                break;
-            case 2:
+        case 1:
+            table.readFile();
+            break;
+        case 2:
+        {
+            sex sSex;
+            cout << "请输入员工姓名 工号 职位 性别：";
+            cin >> sName >> iID >> sPos >> sTmp;
+            if (sTmp == "F")
+                sSex = F;
+            else
+                sSex = M;
+            sEmployee tmp = sEmployee(sName, iID, sPos, sSex);
+            table.addEmployee(tmp);
+            break;
+        }
+        case 3:
+            cout << "请输入工号：";
+            cin >> iID;
+            iPos = table.search(iID);
+            if (iPos >= 0)
+                cout << "找到！\n";
+            else
+                cout << "没找到！\n";
+            break;
+        case 4:
+            cout << "请输入工号";
+            cin >> iID;
+            iPos = table.search(iID);
+            if (iPos >= 0)
             {
-                sex sSex;
-                cout << "请输入员工姓名 工号 职位 性别";
-                cin >> sName >> iID >> sPos >> sTmp;
-                if (sTmp == "F") sSex = F;
-                else sSex = M;
-                sEmployee tmp = sEmployee(sName, iID, sPos, sSex);
-                table.addEmployee(tmp);
-                break;
+                cout << "请输入要变更的职位：";
+                cin >> sTmp;
+                table.changeInfo(iID, sTmp);
             }
-            case 3:
-                cout << "请输入工号";
-                cin >> iID;
-                iPos = table.search(iID);
-                if (iPos >= 0) cout << "找到！\n";
-                else cout << "没找到！\n";
-                break;
-            case 4:
-                cout << "请输入工号";
-                cin >> iID;
-                iPos = table.search(iID);
-                if (iPos >= 0) {
-                    cout << "请输入要变更的职位：";
-                    cin >> sTmp;
-                    table.changeInfo(iID, sTmp);
+            else
+            {
+                cout << "没找到！\n";
+            }
+            break;
+        case 5:
+            cout << "请输入工号：";
+            cin >> iID;
+            iPos = table.search(iID);
+            if (iPos >= 0)
+            {
+                table.deleteEmployee(iID);
+                cout << "已删除！\n";
+            }
+            else
+            {
+                cout << "没找到！\n";
+            }
+            break;
+        case 6:
+            table.print();
+            break;
+        case 7:
+            cout << "请输入查找员工工号：";
+            cin >> iID;
+            int id;
+            int flag = 0;
+            fstream file;
+            file.open("EmployeeInfo2.txt", ios::out | ios::in);
+            while (!file.eof())
+            {
+                file >> id >> sProject >> sPos;
+                if (id == iID && sPos == "sale")
+                {
+                    cout << "担任销售的项目是：" << sProject << endl;
+                    file.close();
+                    flag = 1;
+                    break;
                 }
-                else {
-                    cout << "没找到！\n";
-                }
-                break;
-            case 5:
-                cout << "请输入工号";
-                cin >> iID;
-                iPos = table.search(iID);
-                if (iPos >= 0) {
-                    table.deleteEmployee(iID);
-                    cout << "已删除！\n";
-                }
-                else {
-                    cout << "没找到！\n";
-                }
-                break;
-            case 6:
-                table.print();
-                break;
-            case 7:
-                cout << "请输入查找员工工号：";
-                cin >> iID;
-                int id;
-                fstream file;
-                file.open("EmployeeInfo2.txt", ios::out | ios::in);
-                while (!file.eof()) {
-                    file >> id >> sProject >> sPos;
-                    if (id == iID && sPos == "sale") {
-                        cout << "担任销售的项目是：" << sProject << endl;
-                        file.close();
-                        break;
-                    }
-                }
+            }
+            if (flag == 0)
+            {
                 cout << "没找到这个员工\n";
-                break;
+            }
+            break;
         }
         cmd = printMenu(options);
     }
